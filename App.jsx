4209bbb7608e9d8183e4ebec6e62d3bcb3247f45
Zaeem Rehman <zaeemrehman39@gmail.com>
2025-08-28 16:22:48 +0500

    code change

--- App.jsx
+++ App.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import { View, Button, Text } from 'react-native';
+import { View, Text } from 'react-native';
 
 const App = () => {
   return (
     <View>
       <Text style={ { fontSize : 30 } } >Zaeem Rehman</Text>
       <Text style={ { fontSize : 30 } } >Mobile App Developer</Text>
-      <Button title='Press Here'/>
+      
 
     </View>
   );
