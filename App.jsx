160f7f53799c1eb83f9f813cc407cee9ce04c31a
Zaeem Rehman <zaeemrehman39@gmail.com>
2025-08-28 16:16:51 +0500

    first commit

---
+++ App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { View, Button, Text } from 'react-native';
+
+const App = () => {
+  return (
+    <View>
+      <Text style={ { fontSize : 30 } } >Zaeem Rehman</Text>
+      <Text style={ { fontSize : 30 } } >Mobile App Developer</Text>
+      <Button title='Press Here'/>
+
+    </View>
+  );
+};
+
+export default App
