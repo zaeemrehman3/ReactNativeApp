47dd5e65c07d9840d50a97193e48ce01e8e35f59
Zaeem Rehman <zaeemrehman39@gmail.com>
2025-09-20 13:39:51 +0500

    component added

--- src/components/PressableComponents.jsx
+++ src/components/PressableComponents.jsx
@@ -1,12 +1,59 @@
-import { View, Text } from 'react-native';
+import { View, Text, Pressable, StyleSheet } from 'react-native';
 import React from 'react';
 
 const PressableComponents = () => {
+    // const handlePress = () => {
+    //     console.log('Press On');
+    //     };
+     const handlePressIn = () => {
+        console.log('Press In');
+        };    
+     const handlePressOut = () => {
+        console.log('Press Out');
+        };   
+    const handleLongPress = () => {
+        console.log('Long Press');      
+        //  by default long press value = 500 millisecond
+        };   
   return (
-    <View>
-      <Text style={{fontSize:30}} >PressableComponents</Text>
+    <View style={styles.container} >
+        <Pressable 
+        style={styles.button}
+        // onPress={handlePress}
+        onPressIn={handlePressIn}
+        onPressOut={handlePressOut}
+        onLongPress={handleLongPress}
+        delayLongPress={5000}
+
+        
+        >
+            <Text style={styles.buttonText} >Press Me</Text>
+        </Pressable>
     </View>
   );
 };
 
 export default PressableComponents;
+
+const styles = StyleSheet.create({
+
+    container : {
+        flex : 1,
+        justifyContent : 'center',
+        alignItems : 'center',
+        backgroundColor : '#f5f5f5',
+    },
+
+    button : {
+        padding : 10,
+        borderRadius : 5,
+        backgroundColor : '#007bff',
+    },
+
+    buttonText : {
+        fontSize : 18,
+        color : '#fff',
+    },
+
+
+});
